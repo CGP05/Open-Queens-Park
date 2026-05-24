5ef1151b9fc4be822e84baf8905995b642fb221f
Christian <christian_pasquali@yahoo.com>
2026-05-24 11:01:44 -0400

    requests library test with ola.org

--- scraper.py
+++ scraper.py
@@ -1,1 +1,8 @@
 import requests
+
+url = "https://www.ola.org/en/legislative-business/house-documents/parliament-44/session-1"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text[:500])
